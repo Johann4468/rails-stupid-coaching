7f8b07220f90b9f5e18455dc892deb8acb9f5d20
Le Wagon <johann.lemarie@gmail.com>
2022-11-14 14:08:31 +0100

    Stupid Coaching

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,7 @@
-require "test_helper"
+# frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :headless_chrome # Update this line
 end
